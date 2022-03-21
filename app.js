23196f3a463eb8613a807555beb13b45b08bbd3f
Hassan Yahya A <hassanyahya400@gmail.com>
2022-03-21 10:21:43 +0100

    added the fetch api method

--- app.js
+++ app.js
@@ -1,10 +1,29 @@
 let dice = document.querySelector('.dice');
 let id = document.querySelector('.id-no');
+let adviceContent = document.querySelector('.advice-content');
+const advice_url = 'https://api.adviceslip.com/advice';
 
 
-dice.addEventListener('click', () => {
-     let randomId = Math.floor(Math.random() * 365);
-
-     id.innerHTML = `advice # ${randomId}`
+dice.addEventListener('click', function() {
+     fetch(advice_url)
+          .then(response => response.json())
+          .then(data => {
+               id.innerHTML = `# ${data.slip.id}`;
+               adviceContent.innerHTML = data.slip.advice;
+          })
+          .catch(err => alert('wait for 2sec before before spinning again'))
 });
+
+
+
+
+// async function getAdvice(url) {
+//      const response = await fetch(url);
+//      let data = await response.json();
+
+//      id.innerHTML = `# ${data.slip.id}`;
+//      adviceContent.innerHTML = data.slip.advice;
+
+     
+// }
 
