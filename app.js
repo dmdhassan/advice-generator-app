9e62ecc3ff6fdfb50de7380fd03e0f1d69bde24a
Hassan Yahya A <hassanyahya400@gmail.com>
2022-03-24 13:18:07 +0100

    replace the promise fetch with asynnc await

--- app.js
+++ app.js
@@ -1,29 +1,36 @@
 let dice = document.querySelector('.dice');
 let id = document.querySelector('.id-no');
 let adviceContent = document.querySelector('.advice-content');
-const advice_url = 'https://api.adviceslip.com/advice';
+const url = 'https://api.adviceslip.com/advice';
 
 
-dice.addEventListener('click', function() {
-     fetch(advice_url)
-          .then(response => response.json())
-          .then(data => {
+
+dice.addEventListener('click', () => {
+     getAdvice()
+     .then(data => {  
                id.innerHTML = `# ${data.slip.id}`;
                adviceContent.innerHTML = data.slip.advice;
           })
-          .catch(err => alert('wait for 2sec before before spinning again'))
-});
+          .catch(err => alert('network error, try again!'))
+     })
 
 
+async function getAdvice() {
+     const response = await fetch(url);
+     const data = await response.json();
+
+     return data
+}
+
+//  promise
 
 
-// async function getAdvice(url) {
-//      const response = await fetch(url);
-//      let data = await response.json();
-
-//      id.innerHTML = `# ${data.slip.id}`;
-//      adviceContent.innerHTML = data.slip.advice;
-
-     
-// }
-
+// dice.addEventListener('click', function() {
+//      fetch(url)
+//           .then(response => response.json())
+//           .then(data => {
+//                id.innerHTML = `# ${data.slip.id}`;
+//                adviceContent.innerHTML = data.slip.advice;
+//           })
+//           .catch(err => alert('wait for 2sec before before spinning again'))
+// });
